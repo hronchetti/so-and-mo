f521c7130a8509701aab3d703ef15725a63022f0
Harry Ronchetti <harry@ne6.digital>
2021-03-02 14:07:04 +0000

    nav finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,41 @@
 // Navigation
 
+// Navigation -- Panels
+
 const showNavPanel = (id) => {
   const panel = document.getElementById(id)
   if (panel) {
     panel.classList.add("active")
-    document.getElementById("overlay").classList.add("active")
+    document.getElementById("nav-overlay").classList.add("active")
     document.getElementsByTagName("body")[0].style.overflow = "hidden"
   } else {
-    console.log("Could not find id:" + id)
+    console.log("Could not find id: " + id)
   }
 }
 
 const hideAllNavPanels = () => {
   document.getElementById("mobile-menu").classList.remove("active")
   document.getElementById("basket-drawer").classList.remove("active")
-  document.getElementById("overlay").classList.remove("active")
+  document.getElementById("nav-overlay").classList.remove("active")
   document.getElementsByTagName("body")[0].style.overflow = "visible"
 }
+
+// Navigation -- Desktop Dropdown
+
+const toggleNavDropdownDesktop = () => {
+  document.getElementById("nav-dropdown-desktop").classList.toggle("active")
+}
+
+document.addEventListener("click", (event) => {
+  const dropdown = document.getElementById("nav-dropdown-desktop")
+
+  if (dropdown.classList.contains("active")) {
+    const isClickInside = document
+      .getElementById("nav-dropdown-desktop")
+      .contains(event.target)
+
+    if (!isClickInside) {
+      toggleNavDropdownDesktop()
+    }
+  }
+})
