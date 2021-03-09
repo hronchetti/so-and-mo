6f4c7c1086fb8ef2a62958d634f52291d3b56112
Harry Ronchetti <harry@ne6.digital>
2021-03-09 17:24:46 +0000

    lawn feed product page added

--- src/js/index.js
+++ src/js/index.js
@@ -49,3 +49,96 @@ const getFooterYear = () => {
 document.addEventListener("DOMContentLoaded", () => {
   getFooterYear()
 })
+
+// Products
+
+// Products -- Images Preview
+
+const a = () => {}
+
+// Products -- Lawn Feed -- Modals
+
+const showProductModal = (id) => {
+  const modal = document.getElementById(id)
+
+  if (modal) {
+    modal.classList.add("active")
+    document.getElementById("modal-overlay").classList.add("active")
+    document.getElementsByTagName("body")[0].style.overflow = "hidden"
+  } else {
+    console.log("Could not find id: " + id)
+  }
+}
+
+const hideAllProductModals = () => {
+  document.getElementById("lawn-measurement").classList.remove("active")
+  document.getElementById("check-your-sprayer").classList.remove("active")
+  document.getElementById("modal-overlay").classList.remove("active")
+  document.getElementsByTagName("body")[0].style.overflow = "visible"
+}
+
+// Products -- Lawn Feed -- Bottle Viewer
+
+const viewMonth = (monthButton, month) => {
+  const monthElement = document.getElementById(month)
+  const allMonthElements = document.getElementsByClassName(
+    "bottle-viewer-month-content"
+  )
+  const allMonthButtonElements = document.getElementsByClassName(
+    "bottle-viewer-month-button"
+  )
+
+  if (monthElement) {
+    Array.from(allMonthElements).forEach((currentMonthElement) => {
+      currentMonthElement.classList.remove("active")
+    })
+
+    Array.from(allMonthButtonElements).forEach((currentMonthButtonElement) => {
+      currentMonthButtonElement.classList.remove("active")
+    })
+
+    monthButton.classList.add("active")
+    monthElement.classList.add("active")
+  } else {
+    console.log("Could not find id: " + month)
+  }
+}
+
+// Producst -- Lawn Feed -- Nutrients
+
+const toggleNutrient = (nutrientButton) => {
+  nutrientButton.classList.toggle("active")
+  const nutrientBody = nutrientButton.nextElementSibling
+
+  if (nutrientBody.style.maxHeight) {
+    nutrientBody.style.maxHeight = null
+  } else {
+    nutrientBody.style.maxHeight = nutrientBody.scrollHeight + "px"
+  }
+}
+
+// Accordions
+
+const toggleAccordion = (accordionButton) => {
+  // // Closed state for all accordion-header
+  // const allAccordionButtons = document.getElementsByClassName(
+  //   "accordion-header"
+  // )
+  // Array.from(allAccordionButtons).forEach((currentAccordionButton) => {
+  //   currentAccordionButton.classList.remove("active")
+  // })
+  // // Closed state for all accordion-body
+  // const allAccordionBody = document.getElementsByClassName("accordion-body")
+  // Array.from(allAccordionBody).forEach((currentAccordionBody) => {
+  //   currentAccordionBody.style.maxHeight = null
+  // })
+  // Open clicked accordion
+  accordionButton.classList.toggle("active")
+  const accordionBody = accordionButton.nextElementSibling
+
+  if (accordionBody.style.maxHeight) {
+    accordionBody.style.maxHeight = null
+  } else {
+    accordionBody.style.maxHeight = accordionBody.scrollHeight + "px"
+  }
+}
